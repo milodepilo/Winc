9fbd89b510c395b79a83bd792f06077d0a9d92dd
Milo van Emaus <Milo.van.emaus@gmail.com>
2022-06-16 17:57:52 +0200

    first few functions

--- modules/main.py
+++ modules/main.py
@@ -3,4 +3,29 @@ __winc_id__ = '78029e0e504a49e5b16482a7a23af58c'
 __human_name__ = 'modules'
 
 # Add your code after this line
-def test():
+import this
+import time
+from math import sin
+import datetime
+
+
+def main():
+    # wait(1)
+    print(my_sin(1.5))
+    print(iso_now())
+
+
+def wait(seconds):
+    time.sleep(seconds)
+
+
+def my_sin(float_number):
+    return sin(float_number)
+
+def iso_now():
+    today_date = datetime.datetime.today()
+    iso_date = today_date.isoformat(timespec='minutes')
+    return iso_date
+
+if __name__ == '__main__':
+    main()
