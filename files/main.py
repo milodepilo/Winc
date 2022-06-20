d0487cab3fd55c41cb1f50290666959933eaf5c3
Milo van Emaus <Milo.van.emaus@gmail.com>
2022-06-20 16:02:17 +0200

    fixed code

--- files/main.py
+++ files/main.py
@@ -1,2 +1,55 @@
 __winc_id__ = "ae539110d03e49ea8738fd413ac44ba8"
 __human_name__ = "files"
+
+from functools import cache
+from gettext import find
+import os
+import zipfile
+import pathlib
+
+cache_path = os.path.join(os.getcwd(), 'files\cache')
+zip_path = os.path.join(os.getcwd(), 'files\data.zip')
+
+def main():
+    
+
+    clean_cache()
+    cache_zip(zip_path, cache_path)
+    print(cached_files())
+    print(find_password(cached_files()))
+
+
+def clean_cache():    
+    try:
+        os.mkdir(cache_path)
+    except FileExistsError:            
+            for f in os.listdir(cache_path):
+                os.remove(os.path.join(cache_path, f))
+
+
+def cache_zip(zip_path, cache_path):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(cache_path)
+
+
+def cached_files():
+    list_of_cached_files = []
+    for f in os.listdir(cache_path):
+        list_of_cached_files.append(os.path.join(cache_path, f))        
+    return list_of_cached_files
+
+def find_password(list_of_files):
+    for file_path in list_of_files:           
+        with open(file_path) as f:            
+            read_lines = f.readlines()
+            for line in read_lines:
+                if 'password' in line.lower():                    
+                    return line[line.find(':') + 2:].strip()
+                              
+
+
+
+
+if __name__ == '__main__':
+    main()
+    
