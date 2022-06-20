2ef319dd4491f872804ce403d23509fe8427647e
Milo van Emaus <Milo.van.emaus@gmail.com>
2022-06-20 16:06:11 +0200

    linting

--- files/main.py
+++ files/main.py
@@ -1,28 +1,25 @@
 __winc_id__ = "ae539110d03e49ea8738fd413ac44ba8"
 __human_name__ = "files"
 
-from functools import cache
-from gettext import find
 import os
 import zipfile
-import pathlib
+
 
 cache_path = os.path.join(os.getcwd(), 'files\cache')
 zip_path = os.path.join(os.getcwd(), 'files\data.zip')
 
-def main():
-    
 
+def main():
     clean_cache()
     cache_zip(zip_path, cache_path)
     print(cached_files())
     print(find_password(cached_files()))
 
 
-def clean_cache():    
-    try:
-        os.mkdir(cache_path)
-    except FileExistsError:            
+def clean_cache():
+        try:
+            os.mkdir(cache_path)
+        except FileExistsError:            
             for f in os.listdir(cache_path):
                 os.remove(os.path.join(cache_path, f))
 
@@ -38,16 +35,14 @@ def cached_files():
         list_of_cached_files.append(os.path.join(cache_path, f))        
     return list_of_cached_files
 
+
 def find_password(list_of_files):
     for file_path in list_of_files:           
         with open(file_path) as f:            
             read_lines = f.readlines()
             for line in read_lines:
                 if 'password' in line.lower():                    
-                    return line[line.find(':') + 2:].strip()
-                              
-
-
+                    return line[line.find(':') + 2:].strip()                           
 
 
 if __name__ == '__main__':
